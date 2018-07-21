165779123a7d60399b8bca4f4744bd4a27e7216a
finch185277 <59487andy@gmail.com>
2018-07-21 15:30:38 +0800

    Update Shift_Left_Two_32.v

--- CO_Lab_1/code/Shift_Left_Two_32.v
+++ CO_Lab_1/code/Shift_Left_Two_32.v
@@ -1,19 +1,9 @@
-//Subject:      CO project 1 - Shift_Left_Two_32
-//--------------------------------------------------------------------------------
-//Version:     1
-//--------------------------------------------------------------------------------
-//Description: 
-//--------------------------------------------------------------------------------
+module Shift_Left_Two_32( data_i, data_o );
 
-module Shift_Left_Two_32(
-    data_i,
-    data_o
-    );
-
-//I/O ports                    
+//I/O ports
 input [32-1:0] data_i;
 output [32-1:0] data_o;
 
 //shift left 2
-     
+
 endmodule
