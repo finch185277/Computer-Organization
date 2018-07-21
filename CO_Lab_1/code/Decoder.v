a7b8c4f9981dfd914200ed135e1623fd6468dcd4
finch185277 <59487andy@gmail.com>
2018-07-21 15:27:25 +0800

    Update Decoder.v

--- CO_Lab_1/code/Decoder.v
+++ CO_Lab_1/code/Decoder.v
@@ -1,23 +1,5 @@
-//Subject:     CO project 1 - Decoder
-//--------------------------------------------------------------------------------
-//Version:     1
-//--------------------------------------------------------------------------------
-//Writer:      
-//----------------------------------------------
-//Date:        
-//----------------------------------------------
-//Description: 
-//--------------------------------------------------------------------------------
+module Decoder(instr_op_i,	RegWrite_o,	ALU_op_o,	ALUSrc_o,	RegDst_o,	Branch_o );
 
-module Decoder(
-    instr_op_i,
-	RegWrite_o,
-	ALU_op_o,
-	ALUSrc_o,
-	RegDst_o,
-	Branch_o
-	);
-     
 //I/O ports
 input  [6-1:0] instr_op_i;
 
@@ -26,7 +8,7 @@ output [3-1:0] ALU_op_o;
 output         ALUSrc_o;
 output         RegDst_o;
 output         Branch_o;
- 
+
 //Internal Signals
 reg    [3-1:0] ALU_op_o;
 reg            ALUSrc_o;
@@ -40,10 +22,3 @@ reg            Branch_o;
 //Main function
 
 endmodule
-
-
-
-
-
-                    
-                    
