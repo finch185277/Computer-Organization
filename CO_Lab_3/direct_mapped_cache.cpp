c61664eb87eb260711ed16c8811531a2f2c4d5ac
finch185277 <59487andy@gmail.com>
2018-08-15 18:11:46 +0800

    Update direct_mapped_cache.cpp

--- CO_Lab_3/direct_mapped_cache.cpp
+++ CO_Lab_3/direct_mapped_cache.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdio>
 #include <iostream>
 
@@ -7,46 +8,27 @@ struct cache_content {
   // unsigned int data[16];
 };
 
-// my own log2
-int log2(int n) {
-  int m = 0;
-  while (n >>= 1)
-    ++m;
-  return m;
-}
-
 const int K = 1024;
 
 void simulate(int cache_size, int block_size) {
   unsigned int tag, index, x;
+  unsigned int count = 0, miss = 0;
 
   int offset_bit = log2(block_size);
   int index_bit = log2(cache_size / block_size);
   int line = cache_size >> (offset_bit);
 
-  int no_of_access;
-  int no_of_miss;
-
   cache_content *cache = new cache_content[line];
-  std::cout << "cache_size:" << cache_size << "    ";
-  std::cout << "block_size:" << block_size << std::endl;
-  // std::cout << "cache line:" << line << "    ";
-  // std::cout << "offset_bit:" << offset_bit << "    ";
-  // std::cout << "index_bit:"  << index_bit << std::endl;
 
   for (int j = 0; j < line; j++)
     cache[j].v = false;
 
-  // FILE *fp = std::fopen("DCACHE.txt", "r");
   FILE *fp = std::fopen("ICACHE.txt", "r");
-  if (!fp) {
-    std::cerr << "test file open error!" << std::endl;
-  }
+  // FILE *fp = std::fopen("DCACHE.txt", "r");
+  // FILE *fp = std::fopen("Trace.txt", "r");
 
-  no_of_access = no_of_miss = 0;
   while (std::fscanf(fp, "%x", &x) != EOF) {
-    no_of_access++;
-
+    count++;
     index = (x >> offset_bit) & (line - 1);
     tag = x >> (index_bit + offset_bit);
 
@@ -55,23 +37,22 @@ void simulate(int cache_size, int block_size) {
     } else {
       cache[index].v = true; // miss
       cache[index].tag = tag;
-      no_of_miss++;
+      miss++;
     }
   }
 
   std::fclose(fp);
-
   delete[] cache;
 
-  std::cout << "miss rate:" << (no_of_miss / (double)no_of_access) * 100 << '%'
-            << std::endl;
+  std::cout << "cache_size:" << cache_size << "    ";
+  std::cout << "block_size:" << block_size << '\n';
+  std::cout << "miss rate:" << (miss / (double)count) * 100 << '%' << '\n';
 }
 
 int main() {
-  // Let us simulate 4KB cache with 16B blocks
-  // simulate(1024, 32, 2);
-  for (int i = 64; i <= 512; i <<= 1) {
-    // simulate(i, 0);
+  // cache size range: 32 ~ 256
+  // block size range: 4 ~ 32
+  for (int i = 32; i <= 256; i <<= 1) {
     for (int j = 4; j <= 32; j <<= 1) {
       simulate(i, j);
     }
